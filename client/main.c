f350c70c21e45679283032cb079f9dd6fa2a65e1
Lucas Roberto Raineri Oliveira <lucasrobertoliveira@gmail.com>
2021-08-08 14:04:19 -0300

    Aprendendo a usar o make de forma satisfatoria

--- client/main.c
+++ client/main.c
@@ -2,16 +2,19 @@
 #include <stdio.h>
 
 int main(int argc, char const *argv[]) {
+  printf("Test\n");
+
   tabuleiro localField;
   FILE *fd;
+  printf("Test\n");
 
   argv[0] = "Battleships";
-  if (argc != 0) {
+  if (argc > 0) {
     fd = fopen(argv[1], "r+");
   } else {
-    fd = fopen("predef.field", "r+");
+    fd = fopen("predef.field", "r");
   }
-
+  printf("Test\n");
   init(&localField);
 
   if (!verifyFileIntegrity(fd, &localField)) {
